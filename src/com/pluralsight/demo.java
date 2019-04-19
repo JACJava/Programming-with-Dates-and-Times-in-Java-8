feb1bdbc51f2ec1a1e24f562c9aa297e6e9b19db
Julie Java <juliejava@icloud.com>
2019-04-19 15:35:13 -0500

    Started and Finished Date Programming
    Had to extract from exercise files with jar command

--- src/com/pluralsight/demo.java
+++ src/com/pluralsight/demo.java
@@ -1,14 +1,36 @@
 package com.pluralsight;
 
+import java.util.Calendar;
+
 public class demo {
 
 	public static void main(String[] args) {
 		
-	/***********************************************************************************/
-	/*                                                                                 */
-	/*  20190419 Examples for Programming with Dates and Times in Java 8               */
-	/*                                                                                 */	
-	/***********************************************************************************/	
+		/***********************************************************************************/
+		/*                                                                                 */
+		/*  20190419 Examples for Programming with Dates and Times in Java 8               */
+		/*                                                                                 */	
+		/***********************************************************************************/
+			
+		System.out.println("hello calendar world");
+		
+		// create a calendar
+		
+		//in the Pluralsight demo it said:  Calnedar calendar = new Calendar(0 but gave an error
+		//had to search to see how to do this
+		
+		Calendar calendar = Calendar.getInstance();
+		System.out.println(calendar);
+		
+		//add some tasks to the calendar
+		//calendar.addTask(1,0,"answer email");
+		
+		//add some work periods to the calendar
+		
+		//add an event
+		
+		
+	
 		
 	}
 
---
+++ src/com/pluralsight/testTime.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testTime {
+
+	@Test
+	public void testGenerateWorkPeriods() {
+		
+		
+		fail("Not yet implemented");
+	}
+
+}
